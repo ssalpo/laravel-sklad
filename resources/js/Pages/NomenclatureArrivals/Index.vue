4b210239b7dfd01bc52f7b360857f0c4bd50e302
ssalpo <ssalpomishevich@gmail.com>
2023-03-24 09:21:12 +0500

    управление приход

---
+++ resources/js/Pages/NomenclatureArrivals/Index.vue
@@ -0,0 +1,74 @@
+<template>
+    <Head>
+        <title>Приход</title>
+    </Head>
+
+    <div class="content-header">
+        <div class="container">
+            <h1 class="m-0">Приход</h1>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container">
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-tools">
+                        <Link :href="route('nomenclature-arrivals.create')" class="btn btn-success btn-sm px-3">
+                            Новый приход
+                        </Link>
+                    </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th style="width: 10px">#</th>
+                                <th>Номенклатура</th>
+                                <th>Кол-во</th>
+                                <th>Цена прихода</th>
+                                <th>Цена продажи</th>
+                                <th>Комментарий</th>
+                                <th>Дата прихода</th>
+                                <th width="40"></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="(nomenclatureArrival, index) in nomenclatureArrivals.data">
+                                <td :data-id="nomenclatureArrival.id">{{ ((nomenclatureArrivals.current_page - 1) * nomenclatureArrivals.per_page) + index + 1 }}</td>
+                                <td>{{nomenclatureArrival.nomenclature}}</td>
+                                <td>{{nomenclatureArrival.quantity}} {{nomenclatureArrival.unit}}</td>
+                                <td>{{nomenclatureArrival.price}} {{$page.props.shared.currencyTypeLabelsShort[nomenclatureArrival.currency_type]}}</td>
+                                <td>{{nomenclatureArrival.price_for_sale}} {{$page.props.shared.currencyTypeLabelsShort[nomenclatureArrival.currency_type]}}</td>
+                                <td>{{nomenclatureArrival.comment}}</td>
+                                <td>{{nomenclatureArrival.arrival_at}}</td>
+                                <td class="text-center">
+                                    <Link :href="route('nomenclature-arrivals.edit', nomenclatureArrival.id)">
+                                        <i class="fa fa-pencil-alt"></i>
+                                    </Link>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer clearfix" v-if="nomenclatureArrivals.links.length > 3">
+                    <pagination :links="nomenclatureArrivals.links"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import {Head, Link} from "@inertiajs/inertia-vue3";
+import Pagination from "../../Shared/Pagination.vue";
+
+export default {
+    components: {Pagination, Head, Link},
+    props: ['nomenclatureArrivals'],
+}
+</script>

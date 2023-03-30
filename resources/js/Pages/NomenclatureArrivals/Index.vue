66f892455b8f8b1257f3c04affaa501a6f9c86d0
ssalpo <ssalpomishevich@gmail.com>
2023-03-30 12:58:23 +0500

    фикс

--- resources/js/Pages/NomenclatureArrivals/Index.vue
+++ resources/js/Pages/NomenclatureArrivals/Index.vue
@@ -28,7 +28,6 @@
                                 <th style="width: 10px">#</th>
                                 <th>Номенклатура</th>
                                 <th>Кол-во</th>
-                                <th>Цена прихода</th>
                                 <th>Цена продажи</th>
                                 <th>Комментарий</th>
                                 <th>Дата прихода</th>
@@ -40,7 +39,6 @@
                                 <td :data-id="nomenclatureArrival.id">{{ ((nomenclatureArrivals.current_page - 1) * nomenclatureArrivals.per_page) + index + 1 }}</td>
                                 <td>{{nomenclatureArrival.nomenclature}}</td>
                                 <td>{{nomenclatureArrival.quantity}} {{nomenclatureArrival.unit}}</td>
-                                <td>{{numberFormat(nomenclatureArrival.price)}} сом.</td>
                                 <td>{{numberFormat(nomenclatureArrival.price_for_sale)}} сом.</td>
                                 <td>{{nomenclatureArrival.comment}}</td>
                                 <td>{{nomenclatureArrival.arrival_at}}</td>

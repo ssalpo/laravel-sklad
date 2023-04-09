3b79995bb837254b5f9b01e86219a145716c0e9e
ssalpo <ssalpomishevich@gmail.com>
2023-04-10 04:11:56 +0500

    поправил переход в админке через нотификации телеграм

--- app/Notifications/NewOrder.php
+++ app/Notifications/NewOrder.php
@@ -64,7 +64,7 @@ class NewOrder extends Notification implements ShouldQueue
             $m->line('----');
         }
 
-        $m->button('Просмотреть', url()->route('orders.show', $this->order->id));
+        $m->button('Просмотреть', dd(url('http://sk.94.ru') . '/orders/' . $this->order->id));
 
         return $m;
     }
